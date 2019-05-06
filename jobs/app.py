23a12ea41de164fbf5adb1a0154c0f161744c93e
itsjessie <44765634+itsjessie@users.noreply.github.com>
2019-05-07 09:47:52 +1000

    Update app.py

---
+++ jobs/app.py
@@ -0,0 +1,10 @@
+mport flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/jobs')
+# an additional route decorator, so jobs can be reached through '/' and '/jobs'.
+def jobs():
+    return render_template(index.html)
